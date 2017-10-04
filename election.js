9018913ce9266b34f4f7ba56cea177791de8b513
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-10-04 16:27:58 -0400

    done

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function(responseData){
+    var c = responseData.candidates;
+    for (var i=0; i<c.length; i++) {
+     var ul = document.querySelector('ul');
+     var list = document.createElement('li');
+     list.innerText = 'Name '+ c[i].name + ',' + 'votes ' + c[i].votes;
+     ul.appendChild(list);
+}});
+
 
 });
